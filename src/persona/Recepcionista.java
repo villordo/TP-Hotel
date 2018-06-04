bf8e72ff49d959d57645cbc5b30fb50b954621e8
Jorge Villordo <villordo.mdq@live.com.ar>
2018-06-04 20:36:43 -0300

    Commit 04/06

--- src/persona/Recepcionista.java
+++ src/persona/Recepcionista.java
@@ -45,15 +45,27 @@ public void mostrarHabDisponibles(ArrayList<Habitacion> list)
 
 	}
 }
+/**
+ * Crea una reserva y lo agrega al arrar de reservas.
+ * @param reserva : array que contiene las habitaciones que ocupara dicha Reserva.
+ * @param fecha_inic : fecha en el que inicia la Reserva.
+ * @param fecha_fin : fecha en que se finaliza la Reserva.
+ * @param idCliente : es el ID del cliente que realizara la Reserva.
+ * @return : el id de la reserva.
+ */
 public int hacerUnaReserva(ArrayList<Habitacion> reserva,Date fecha_inic,Date fecha_fin,int idCliente)
 {
 	return 2;
 }
-
-public ArrayList<Habitacion> elegirHabitaciones(ArrayList<Habitaciones> habDisponibles,int cantPersonas)
+/**
+ * Crea un ArrayList con las habitaciones elegidas por el Usuario y las retorna. 
+ * @param habDisponibles : es un array que contiene las habitaciones disponibles.
+ * @param cantPersonas : cantidad de personas que se desean alojar en el Hotel.
+ * @return un ArrayList con las habitaciones elegidas por el Usuario.
+ */
+public ArrayList<String> elegirHabitaciones(ArrayList<Habitaciones> habDisponibles,int cantPersonas)
 {
 	
 }
-
 
 }
